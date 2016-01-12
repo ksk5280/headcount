402424fa1d7f4b7f3b7d5b1fdf5c15dd77c10742
David Whitaker <damwhit@gmail.com>
2016-01-12 13:38:49 -0700

    parsed out dataloader

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,21 +1,14 @@
 require 'csv'
-require 'pry'
 require 'set'
 require 'district'
+require 'data_loader'
 
 class DistrictRepository
-  attr_reader :districts,
-              :unique_data
+  attr_reader :districts
 
 
   def load_data(data)
-    data = CSV.open "#{data.fetch(:enrollment).fetch(:kindergarten)}", headers: true, header_converters: :symbol
-    @districts = Set.new
-
-    data.each do |row|
-    districts << row[:location].upcase
-    end
-    # districts is a set of unique district names
+    @districts = DataLoader.new.load_csv(data)
   end
 
   def find_by_name(name)
@@ -37,8 +30,3 @@ class DistrictRepository
   end
 
 end
-#
-# dr = DistrictRepository.new
-# dr.load_data({:enrollment => {
-#                           :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-#                         })
--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,21 +1,14 @@
 require 'csv'
-require 'pry'
 require 'set'
 require 'enrollment'
+require 'data_loader'
 
 class EnrollmentRepository
-  attr_reader :districts,
-              :unique_data
+  attr_reader :districts
 
 
   def load_data(data)
-    data = CSV.open "#{data.fetch(:enrollment).fetch(:kindergarten)}", headers: true, header_converters: :symbol
-    @districts = Set.new
-
-    data.each do |row|
-    districts << row[:location].upcase
-    end
-    # districts is a set of unique district names
+    @districts = DataLoader.new.load_csv(data)
   end
 
   def find_by_name(name)
