7337bd3b81198de920d6c932df60040eb4d99e0e
Kimiko Kano <kskano@gmail.com>
2016-01-13 12:50:41 -0700

    refactored and added edge cases for iter_0

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -14,9 +14,9 @@ class Enrollment
 
   def kindergarten_participation_by_year
     kindergarten_participation.each do |k, v|
-      kindergarten_participation[k] = (v * 1000).floor / 1000.0
+      kindergarten_participation[k] = v.round(3)
     end
-    #returns a hash with keys as years and values as truncated percentage
+    # returns a hash with keys as years and values as truncated percentage
   end
 
   def kindergarten_participation_in_year(year)
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -2,47 +2,65 @@ require 'district_repository'
 require 'test_helper'
 
 class DistrictRepositoryTest < Minitest::Test
+  # def setup
+  #   @dr = DistrictRepository.new
+  #   @districts = dr.load_data({
+  #     :enrollment => {
+  #       :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+  #     }
+  #   })
+  # end
 
   def test_it_can_take_in_a_file
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     assert_equal 2, dr.districts.count
   end
 
   def test_it_can_find_by_name
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district = dr.find_by_name("ACADEMY 20")
     assert_equal "ACADEMY 20", district.name
   end
 
   def test_it_can_find_by_name_lower_case_names
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district = dr.find_by_name("Colorado")
     assert_equal "COLORADO", district.name
   end
 
-  def test_returns_nil_if_district_is_not_found
+  def test_find_by_name_returns_nil_if_district_is_not_found
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district = dr.find_by_name("PIZZA")
     assert_nil district
   end
 
   def test_it_can_find_by_name_and_create_an_instance_of_an_object
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district = dr.find_by_name("ACADEMY 20")
     msg = "Expected district to be a class of District"
     assert district.instance_of?(District), msg
@@ -50,37 +68,53 @@ class DistrictRepositoryTest < Minitest::Test
 
   def test_finds_all_matching_districts_using_name_fragment
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district_array = dr.find_all_matching("ACA")
     assert_equal ["ACADEMY 20"], district_array
   end
 
   def test_returns_empty_array_if_no_matches_are_found
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
-    district_array = dr.find_all_matching("dadsfasfafasasfdasfdsa")
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
+    district_array = dr.find_all_matching("dsfas")
     assert_equal [], district_array
   end
 
   def test_finds_all_matching_districts_using_lowercase_name_fragment
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district_array = dr.find_all_matching("aca")
     assert_equal ["ACADEMY 20"], district_array
   end
 
   def test_result_is_upcased_even_if_original_location_is_not
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {
-                    :kindergarten => "test/fixtures/kindergarten_fixture.csv"}
-                  })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "test/fixtures/kindergarten_fixture.csv"
+      }
+    })
     district_array = dr.find_all_matching("colo")
     assert_equal ["COLORADO"], district_array
+  end
+
+  def test_what_happens_if_there_is_no_enrollment_key_for_load_data
+    skip
+  end
+
+  def test_that_enrollment_repo_instance_is_created_when_data_is_loaded
+    skip
   end
 end
